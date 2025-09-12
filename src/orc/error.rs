46b3ed851fa0b7865af1ec8d29df7ad04eb43706
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-09-11 18:40:15 -0700

    More errors.

--- src/orc/error.rs
+++ src/orc/error.rs
@@ -1,9 +1,9 @@
-use std::{ffi::CStr, rc::Rc};
+use std::{ffi::{CStr, NulError}, rc::Rc};
 
 use libc::strlen;
 use llvm_sys::error::{LLVMDisposeErrorMessage, LLVMGetErrorMessage, LLVMOpaqueError};
 
-use crate::error::LLVMErrorString;
+use crate::{error::LLVMErrorString, support::LLVMString};
 
 /// Handles an [LLVMOpaqueError] that was issued from LLVM.
 pub(crate) fn handle_llvm_error_message(opaque: *mut LLVMOpaqueError) -> Result<(), LLVMErrorString> {
@@ -15,14 +15,24 @@ pub(crate) fn handle_llvm_error_message(opaque: *mut LLVMOpaqueError) -> Result<
 }
 
 // TODOC (ErisianArchitect): enum OrcError
-#[derive(Debug, Clone, thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum OrcError {
     #[error("Failed to create Orc JIT engine.")]
     CreateInstanceFailure,
-    #[error("Failed to create eagerly compiled IR module.")]
-    AddEagerlyCompiledIRFailure,
+    #[error("Failed to create eagerly compiled IR module: {0}")]
+    AddEagerlyCompiledIRFailure(LLVMErrorString),
     #[error("Failed to create lazily compiled IR module.")]
-    AddLazilyCompiledIRFailure,
+    AddLazilyCompiledIRFailure(LLVMErrorString),
+    #[error("Failed to add object file.")]
+    AddObjectFileFailure(LLVMErrorString),
+    #[error("Failed to lookup symbol address: {0}")]
+    SymbolAddressLookupFailure(LLVMErrorString),
+    #[error("The module was already owned by an execution engine.")]
+    ModuleOwnedByExecutionEngine,
     #[error("LLVM Error: {0}")]
     LLVMError(#[from] LLVMErrorString),
+    #[error("LLVM: {0}")]
+    LLVMString(#[from] LLVMString),
+    #[error("NulError: {0}")]
+    NulError(#[from] NulError),
 }
