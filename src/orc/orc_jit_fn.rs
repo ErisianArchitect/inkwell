33252db1c7e167974623070f5442f36b8fda7150
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-09-13 14:31:29 -0700

    Renaming, comments, formatting.

--- src/orc/orc_jit_fn.rs
+++ src/orc/orc_jit_fn.rs
@@ -5,15 +5,15 @@ mod private {
     pub trait SealedUnsafeOrcJitFnPtr: Copy {}
 }
 
-pub trait UnsafeOrcJitFnPtr: private::SealedUnsafeOrcJitFnPtr {}
+pub trait UnsafeOrcFn: private::SealedUnsafeOrcJitFnPtr {}
 
 #[derive(Debug, Clone)]
-pub struct OrcJitFunction<'ctx, F> {
+pub struct OrcFunction<'ctx, F> {
     inner: F,
     _phantom: PhantomData<&'ctx ()>,
 }
 
-impl<'ctx, F> OrcJitFunction<'ctx, F> {
+impl<'ctx, F> OrcFunction<'ctx, F> {
     #[must_use]
     #[inline]
     pub(crate) const fn new(function: F) -> Self {
@@ -24,6 +24,9 @@ impl<'ctx, F> OrcJitFunction<'ctx, F> {
     }
 }
 
+// code yoinked from execution_engine.rs
+// Hmm. Maybe the two should be merged? Either way, I thought it would be nice to have more parameters.
+// the number of parameters can be reduced if it becomes a compilation bottleneck.
 macro_rules! impl_unsafe_fn {
     (@recurse $first:ident $( , $rest:ident )*) => {
         impl_unsafe_fn!($( $rest ),*);
@@ -33,9 +36,9 @@ macro_rules! impl_unsafe_fn {
     
     ($( $param:ident ),*) => {
         impl<Output, $( $param ),*> private::SealedUnsafeOrcJitFnPtr for unsafe extern "C" fn($( $param ),*) -> Output {}
-        impl<Output, $( $param ),*> UnsafeOrcJitFnPtr for unsafe extern "C" fn($( $param ),*) -> Output {}
+        impl<Output, $( $param ),*> UnsafeOrcFn for unsafe extern "C" fn($( $param ),*) -> Output {}
 
-        impl<Output, $( $param ),*> OrcJitFunction<'_, unsafe extern "C" fn($( $param ),*) -> Output> {
+        impl<Output, $( $param ),*> OrcFunction<'_, unsafe extern "C" fn($( $param ),*) -> Output> {
             /// This method allows you to call the underlying function while making
             /// sure that the backing storage is not dropped too early and
             /// preserves the `unsafe` marker for any calls.
@@ -50,11 +53,16 @@ macro_rules! impl_unsafe_fn {
     };
 }
 
-impl_unsafe_fn!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29, T30, T31);
+impl_unsafe_fn!(
+    T0, T1, T2, T3, T4, T5, T6, T7,
+    T8, T9, T10, T11, T12, T13, T14, T15,
+    T16, T17, T18, T19, T20, T21, T22, T23,
+    T24, T25, T26, T27, T28, T29, T30, T31
+);
 
 #[macro_export]
 macro_rules! orc_jit_fn_ty {
     (fn($($param:ty),*$(,)?)$( -> $result:ty)?) => {
-        $crate::orc::orc_jit_fn::OrcJitFunction::<unsafe extern "C" fn($($param),*) $( -> $result )?>
+        $crate::orc::orc_jit_fn::OrcFunction::<unsafe extern "C" fn($($param),*) $( -> $result )?>
     };
 }
