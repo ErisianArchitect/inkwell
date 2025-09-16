ae1a43bfa810b30b09c7e832193239240431ab67
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-09-15 21:34:15 -0700

    Various changes. Forgot to track them.

--- src/orc/function_address.rs
+++ src/orc/function_address.rs
@@ -4,48 +4,58 @@ use llvm_sys::orc::LLVMOrcTargetAddress;
 
 use crate::orc::orc_jit_fn::UnsafeOrcFn;
 
-
-
 // TODOC (ErisianArchitect): struct FunctionAddress
 #[repr(transparent)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct FunctionAddress {
-    pub(crate) address: LLVMOrcTargetAddress
+    pub(crate) raw: LLVMOrcTargetAddress
 }
+
+// SAFETY: LLVMOrcTargetAddress should just be a primitive type, and should be safe to send across thread boundaries.
+//         These traits might be auto-implemented, but I figured it would be a good idea to force their implementation
+//         just in case.
+unsafe impl Send for FunctionAddress {}
+unsafe impl Sync for FunctionAddress {}
 
 // TODOC (ErisianArchitect): impl FunctionAddress
 impl FunctionAddress {
-    pub const NULL: Self = Self::null();
+    pub const NULL: Self = Self { raw: 0 };
+    
+    // Either NULL or null can be removed, but having them both gives users options for when they prefer a function
+    // over a const. Everyone hates CAPS LOCK. Also, it means that you can provide `FunctionAddress::null` as input
+    // to something that expects a function that returns FunctionAddress.
     #[must_use]
     #[inline]
-    pub fn new<F: UnsafeOrcFn>(function: F) -> Self {
-        let address: usize = unsafe { transmute_copy(&function) };
-        unsafe { Self::from_raw(address as LLVMOrcTargetAddress) }
+    pub const fn null() -> Self {
+        Self::NULL
     }
     
     /// Unsafe fallback for functions that cannot be represented with `UnsafeOrcFn`.
     #[must_use]
     #[inline]
-    pub const unsafe fn from_raw(address: LLVMOrcTargetAddress) -> Self {
+    pub unsafe fn from_raw(address: LLVMOrcTargetAddress) -> Self {
         Self {
-            address
+            raw: address
         }
     }
     
     #[must_use]
     #[inline]
-    pub const fn null() -> Self {
-        Self { address: 0 }
+    pub fn new<F: UnsafeOrcFn>(function: F) -> Self {
+        // SAFETY: UnsafeOrcFn is only implemented for function pointers, so transmuting to `usize` is a valid
+        //         operation.
+        let address: usize = unsafe { transmute_copy(&function) };
+        unsafe { Self::from_raw(address as _) }
     }
     
+    /// Gets the raw [LLVMOrcTargetAddress] that this [FunctionAddress] wraps.
     #[must_use]
     #[inline]
-    pub fn address(self) -> LLVMOrcTargetAddress {
-        self.address
+    pub fn raw(self) -> LLVMOrcTargetAddress {
+        self.raw
     }
 }
 
-// This macro is not strictly necessary, but it makes coercion easier and less verbose.
 /// Utility macro to create a [FunctionAddress] with less verbosity. This handles the coercion for you.
 /// # Example
 /// ```rust, no_run
