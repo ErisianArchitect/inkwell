9a60b07a47e9efd649d9f15ac9696490801a3a2d
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-09-17 03:40:46 -0700

    Removed push and then renamed push_raw to push.

--- src/lockfree_linked_list.rs
+++ src/lockfree_linked_list.rs
@@ -33,7 +33,7 @@ impl<T> LockfreeLinkedList<T> {
         }
     }
     
-    pub unsafe fn push_raw(
+    pub unsafe fn push(
         &self,
         next: &AtomicPtr<LockfreeLinkedListNode<T>>,
         node: *const LockfreeLinkedListNode<T>,
@@ -46,17 +46,6 @@ impl<T> LockfreeLinkedList<T> {
                 Err(new_head) => new_head,
             };
         }
-    }
-    
-    #[must_use]
-    pub fn push(&self, value: T) -> *mut T {
-        // let mut old_head = self.head.load(Ordering::Acquire);
-        let node = LockfreeLinkedListNode::new(value);
-        unsafe { self.push_raw(&node.next, &*node); }
-        // LockfreeLinkedListNode<T> is repr(C) with T as the first field.
-        // That means that a pointer to LockfreeLinkedListNode<T> is also a pointer to T.
-        // leak the node so that it can be cleaned up later.
-        Box::into_raw(node).cast()
     }
 }
 
