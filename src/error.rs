52c9cb8b27a2cf3df5e3b633b534e04b69d5281b
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-08-20 15:42:13 -0700

    Implementation of LLVMErrorMessage

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,24 @@
 
 use ::core::{
     ptr::{
+        self,
         NonNull,
+    },
+    mem::{
+        ManuallyDrop,
+    },
+    ffi::{
+        c_char,
+        CStr,
+    },
+    convert::{
+        Infallible,
+    },
+};
+
+use ::std::{
+    borrow::{
+        Cow,
     },
 };
 
@@ -12,6 +29,8 @@ use ::llvm_sys::{
         LLVMConsumeError,
         LLVMErrorTypeId,
         LLVMGetErrorTypeId,
+        LLVMGetErrorMessage,
+        LLVMDisposeErrorMessage,
     },
 };
 
@@ -46,6 +65,74 @@ pub enum Error {
     GlobalMetadataError,
 }
 
+/// An error message produced by [LLVMError]. This error message can not be expected to be valid utf-8, because it might
+/// be constructed from a POSIX-path or some other sequence that contains near-arbitrary bytes (except for
+/// nul-terminators).
+#[repr(C)]
+#[derive(Debug)]
+pub struct LLVMErrorMessage {
+    ptr: NonNull<c_char>,
+    len: usize,
+}
+const _: () = crate::support::assert_niche::<LLVMErrorMessage>();
+
+impl LLVMErrorMessage {
+    /// Converts the error message into a raw c-string pointer.
+    #[must_use]
+    #[inline(always)]
+    pub fn as_ptr(&self) -> *const c_char {
+        self.ptr.as_ptr()
+    }
+
+    /// Returns the length of the error message.
+    #[must_use]
+    #[inline(always)]
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    /// Returns the sequence of raws bytes up until the nul-terminator.
+    #[must_use]
+    #[inline(always)]
+    pub fn as_bytes(&self) -> &[u8] {
+        unsafe {
+            ::core::slice::from_raw_parts(self.as_ptr().cast(), self.len)
+        }
+    }
+
+    /// Returns the sequence of raw bytes that also contains the nul-terminator.
+    #[must_use]
+    #[inline(always)]
+    pub fn as_bytes_with_nul(&self) -> &[u8] {
+        unsafe {
+            ::core::slice::from_raw_parts(self.as_ptr().cast(), self.len + 1)
+        }
+    }
+
+    /// Produce a [CStr] from this errro message.
+    #[must_use]
+    #[inline(always)]
+    pub fn as_cstr(&self) -> &CStr {
+        unsafe {
+            CStr::from_bytes_with_nul_unchecked(self.as_bytes_with_nul())
+        }
+    }
+
+    /// Convert to a utf-8 string using a lossy operation.
+    /// Since [LLVMErrorMessage] might be a non-utf8 string, it is necessary to build a lossy utf-8 string from it.
+    pub fn to_string_lossy(&self) -> Cow<'_, str> {
+        String::from_utf8_lossy(self.as_bytes())
+    }
+}
+
+impl Drop for LLVMErrorMessage {
+    fn drop(&mut self) {
+        unsafe {
+            LLVMDisposeErrorMessage(self.as_ptr().cast_mut());
+        }
+    }
+}
+
 // [https://llvm.org/doxygen/classllvm_1_1Error.html]
 /// Internal LLVM error type with context and mandatory checking.
 /// 
@@ -58,6 +145,8 @@ pub struct LLVMError {
 const _: () = crate::support::assert_niche::<LLVMError>();
 
 impl LLVMError {
+    /// Pass an [LLVMErrorRef] returned from an LLVM function. You do not need to perform a null check on the
+    /// [LLVMErrorRef], this function does so for you, and returns `None` if it was null.
     pub fn from_error_ref(error: LLVMErrorRef) -> Option<Self> {
         NonNull::new(error)
             .map(|ptr| Self { ptr })
@@ -77,11 +166,46 @@ impl LLVMError {
     /// This simply drops the [LLVMError], which calls [LLVMConsumeError].
     pub fn consume(self) {}
 
+    /// Obtains the class ID of this error, or null if this is a success value.
     pub fn get_type_id(&self) -> LLVMErrorTypeId {
         unsafe {
             LLVMGetErrorTypeId(self.as_ptr())
         }
     }
+
+    /// Consume the error by turning it into an [LLVMErrorMessage].
+    pub fn take_message(self) -> LLVMErrorMessage {
+        unsafe {
+            let error = ManuallyDrop::new(self);
+            let cstr_ptr = LLVMGetErrorMessage(error.as_ptr());
+            let Some(ptr) = NonNull::new(cstr_ptr) else {
+                crate::support::panic_out_of_memory_error(file!(), line!(), "Could not get LLVMErrorMessage.")
+            };
+            let cstr = CStr::from_ptr(cstr_ptr);
+            let len = cstr.count_bytes();
+            LLVMErrorMessage {
+                ptr,
+                len,
+            }
+        }
+    }
+
+    // TODO (ErisianArchitect): Determine if this function is even necessary.
+    /// For operations that return an [LLVMError] but are expected to never fail, you should use this function to
+    /// handle the returned error. This function will invoke a *panic* if there *is* a failure, so you should be mindful
+    /// of how you utilize it. This is an alternative to [llvm_sys::LLVMCantFail], which would typically abort.
+    ///
+    /// # NOTE
+    /// This function can panic if the [LLVMErrorRef] points to a failure.
+    pub(crate) fn infallible(error_ref: LLVMErrorRef) {
+        let Some(err) = Self::from_error_ref(error_ref) else {
+            return;
+        };
+        let msg = err.take_message();
+        let str_msg = msg.to_string_lossy();
+        panic!("Infallible operation was not infallible: {}\nFile: {}:{}", str_msg, file!(), line!());
+    }
+
 }
 
 impl Drop for LLVMError {
