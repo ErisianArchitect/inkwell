46e3e9c3838733dfd74ffefb862197ea3871511e
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-08-19 20:52:35 -0700

    Added note on version availability.

--- src/orc2/mod.rs
+++ src/orc2/mod.rs
@@ -3,5 +3,16 @@ pub mod lljit;
 pub mod target_machine;
 
 /* ---| NOTES:
-
+Available on versions:
+- llvm12-0
+- llvm13-0
+- llvm14-0
+- llvm15-0
+- llvm16-0
+- llvm17-0
+- llvm18-1
+- llvm19-1
+- llvm20-1
+- llvm21-1
+- llvm22-1
 */
