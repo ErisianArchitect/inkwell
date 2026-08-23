e8973bc9bd6a4aaa95447a6d56676a6f9a60d4d3
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-08-23 14:36:12 -0700

    Add from_raw_unchecked() and from_raw() to ThreadSafeModule.

--- src/orc2/thread_safe_module.rs
+++ src/orc2/thread_safe_module.rs
@@ -20,6 +20,31 @@ pub struct ThreadSafeModule {
 }
 
 impl ThreadSafeModule {
+    
+    /// Create an [ThreadSafeModule] instance from a raw [LLVMOrcThreadSafeModuleRef] without checking if it is null.
+    ///
+    /// # Safety
+    /// Will cause Undefined Behavior if the pointer is either null, or does not point to a valid
+    /// [LLVMOrcThreadSafeModuleRef].
+    pub fn from_raw_unchecked(ptr: LLVMOrcThreadSafeModuleRef) -> Self {
+        unsafe {
+            Self {
+                ptr: NonNull::new_unchecked(ptr),
+            }
+        }
+    }
+
+    /// Create an [ThreadSafeModule] instance from a raw [LLVMOrcThreadSafeModuleRef]. This function will only
+    /// ensure that the pointer is non-null, it cannot verify that the reference is valid.
+    ///
+    /// # Safety
+    /// Will cause Undefined Behavior if the pointer does not point to a valid [LLVMOrcThreadSafeModuleRef].
+    pub fn from_raw(ptr: LLVMOrcThreadSafeModuleRef) -> Option<Self> {
+        Some(Self {
+            ptr: NonNull::new(ptr)?,
+        })
+    }
+    
     #[must_use]
     #[inline(always)]
     pub fn as_ptr(&self) -> LLVMOrcThreadSafeModuleRef {
