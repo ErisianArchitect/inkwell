b7227808f74c2f1ff08a48d5a05832ff216d1b36
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-08-22 20:31:37 -0700

    Added from_raw_unchecked() and from_raw() functions to DumpObjects.

--- src/orc2/dump_objects.rs
+++ src/orc2/dump_objects.rs
@@ -1,1 +1,42 @@
 
+use ::core::{
+    ptr::{
+        NonNull,
+    },
+};
+
+use ::llvm_sys::{
+    orc2::{
+        LLVMOrcDumpObjectsRef,
+        LLVMOrcOpaqueDumpObjects,
+        LLVMOrcCreateDumpObjects,
+    },
+};
+
+#[repr(transparent)]
+#[derive(Debug)]
+pub struct DumpObjects {
+    ptr: NonNull<LLVMOrcOpaqueDumpObjects>,
+}
+
+impl DumpObjects {
+    /// Create an [DumpObjects] instance from a raw [LLVMOrcDumpObjectsRef] without checking if it is null.
+    ///
+    /// # Safety
+    /// Will cause Undefined Behavior if the pointer is either null, or does not point to a valid
+    /// [LLVMOrcDumpObjectsRef].
+    pub unsafe fn from_raw_unchecked(ptr: LLVMOrcDumpObjectsRef) -> Self {
+        unsafe { Self { ptr: NonNull::new_unchecked(ptr) } }
+    }
+
+    /// Create an [DumpObjects] instance from a raw [LLVMOrcDumpObjectsRef]. This function will only ensure that the
+    /// pointer is non-null, it cannot verify that the reference is valid.
+    ///
+    /// # Safety
+    /// Will cause Undefined Behavior if the pointer does not point to a valid [LLVMOrcDumpObjectsRef].
+    pub unsafe fn from_raw(ptr: LLVMOrcDumpObjectsRef) -> Option<Self> {
+        Some(Self {
+            ptr: NonNull::new(ptr)?,
+        })
+    }
+}
