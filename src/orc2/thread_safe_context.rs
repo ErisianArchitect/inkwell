1cb5df6deb1c0e13613af3476ee33a026277c442
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-08-23 21:39:23 -0700

    Isolate unsafe block to unsafe function call.

--- src/orc2/thread_safe_context.rs
+++ src/orc2/thread_safe_context.rs
@@ -97,23 +97,21 @@ impl ThreadSafeContext {
     /// Create a new [ThreadSafeContext] from an existing [Context]. This will take ownership of the [Context].
     #[must_use]
     pub fn from_context(context: Context) -> Self {
-        unsafe {
-            // C API source code:
-            // [https://llvm.org/doxygen/OrcV2CBindings_8cpp_source.html#l00735]
-            // Documentation:
-            // [https://llvm.org/doxygen/group__LLVMCExecutionEngineORC.html#ga51863e392b2346f7bce719f3faf8fc12]
-            let context = ManuallyDrop::new(context);
-            let ptr = LLVMOrcCreateNewThreadSafeContextFromLLVMContext(context.raw());
-            let Some(ptr) = NonNull::new(ptr) else {
-                crate::support::panic_out_of_memory_error(
-                    file!(),
-                    line!(),
-                    "Unable to create ThreadSafeContext from Context.",
-                );
-            };
-            Self {
-                ptr,
-            }
+        // C API source code:
+        // [https://llvm.org/doxygen/OrcV2CBindings_8cpp_source.html#l00735]
+        // Documentation:
+        // [https://llvm.org/doxygen/group__LLVMCExecutionEngineORC.html#ga51863e392b2346f7bce719f3faf8fc12]
+        let context = ManuallyDrop::new(context);
+        let ptr = unsafe { LLVMOrcCreateNewThreadSafeContextFromLLVMContext(context.raw()) };
+        let Some(ptr) = NonNull::new(ptr) else {
+            crate::support::panic_out_of_memory_error(
+                file!(),
+                line!(),
+                "Unable to create ThreadSafeContext from Context.",
+            );
+        };
+        Self {
+            ptr,
         }
     }
 
