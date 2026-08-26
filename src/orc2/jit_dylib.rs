d0e47e1bd63eedeb43c82995fb4300e911bf6a43
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-08-25 19:20:20 -0700

    Add JitDylibRef impl with from_raw_unchecked(), from_raw(), and as_ptr() functions.

--- src/orc2/jit_dylib.rs
+++ src/orc2/jit_dylib.rs
@@ -31,3 +31,43 @@ pub struct JITDylibRef<'lljit> {
     // [https://llvm.org/docs/doxygen/llvm-c_2LLJIT_8h_source.html#l00144]
     pub(crate) _phantom: PhantomData<(&'lljit LLJIT,)>
 }
+
+impl<'lljit> JITDylibRef<'lljit> {
+    /// Create a [JITDylibRef] instance from a raw [LLVMOrcJITDylibRef] without checking if it is null.
+    ///
+    /// # Safety
+    /// Will cause Undefined Behavior if the pointer is either null, or does not point to a valid
+    /// [LLVMOrcJITDylibRef].
+    #[must_use]
+    #[inline(always)]
+    pub unsafe fn from_raw_unchekced(ptr: LLVMOrcJITDylibRef) -> Self {
+        Self {
+            ptr: unsafe { NonNull::new_unchecked(ptr) },
+            _phantom: PhantomData,
+        }
+    }
+
+    /// Create a [JITDylibRef] instance from a raw [LLVMOrcJITDylibRef]. This function will only ensure that the
+    /// pointer is non-null, it cannot verify that the reference is valid.
+    ///
+    /// # Safety
+    /// Will cause Undefined Behavior if the pointer does not point to a valid [LLVMOrcJITDylibRef].
+    #[must_use]
+    #[inline]
+    pub unsafe fn from_raw(ptr: LLVMOrcJITDylibRef) -> Option<Self> {
+        Some(Self {
+            ptr: NonNull::new(ptr)?,
+            _phantom: PhantomData,
+        })
+    }
+
+    /// Returns the inner [LLVMOrcJITDylibRef].
+    ///
+    /// # NOTE
+    /// [JitDylibRef] is a transparent wrapper around this pointer.
+    #[must_use]
+    #[inline(always)]
+    pub fn as_ptr(&self) -> LLVMOrcJITDylibRef {
+        self.ptr.as_ptr()
+    }
+}
