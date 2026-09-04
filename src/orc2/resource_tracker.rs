df3277e320f55ecb07d974a61eba962c71cb6bc8
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-09-04 02:42:28 -0700

    Add from_raw_unchecked, from_raw, and as_ptr to ResourceTracker.

--- src/orc2/resource_tracker.rs
+++ src/orc2/resource_tracker.rs
@@ -20,4 +20,39 @@ pub struct ResourceTracker {
 }
 
 impl ResourceTracker {
+    /// Create a [ResourceTracker] instance from a raw [LLVMOrcResourceTrackerRef] without checking if it is null.
+    ///
+    /// # Safety
+    /// Will cause Undefined Behavior if the pointer is either null, or does not point to a valid
+    /// [LLVMOrcResourceTrackerRef].
+    #[must_use]
+    #[inline(always)]
+    pub unsafe fn from_raw_unchekced(ptr: LLVMOrcResourceTrackerRef) -> Self {
+        Self {
+            ptr: unsafe { NonNull::new_unchecked(ptr) },
+        }
+    }
+
+    /// Create a [ResourceTracker] instance from a raw [LLVMOrcResourceTrackerRef]. This function will only ensure that the
+    /// pointer is non-null, it cannot verify that the reference is valid.
+    ///
+    /// # Safety
+    /// Will cause Undefined Behavior if the pointer does not point to a valid [LLVMOrcResourceTrackerRef].
+    #[must_use]
+    #[inline]
+    pub unsafe fn from_raw(ptr: LLVMOrcResourceTrackerRef) -> Option<Self> {
+        Some(Self {
+            ptr: NonNull::new(ptr)?,
+        })
+    }
+
+    /// Returns the inner [LLVMOrcResourceTrackerRef].
+    ///
+    /// # NOTE
+    /// [ResourceTracker] is a transparent wrapper around this pointer.
+    #[must_use]
+    #[inline(always)]
+    pub fn as_ptr(&self) -> LLVMOrcResourceTrackerRef {
+        self.ptr.as_ptr()
+    }
 }
